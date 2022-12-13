747e5a095d16192a0f8ae6dbd71dc1dd5240646d
Rwpulley <rwpull@gmail.com>
2022-12-12 21:51:22 -0600

    Fixed more small details

--- CarpetCleaning/StoredProcedures/Dbo.validateexceldata.sql
+++ CarpetCleaning/StoredProcedures/Dbo.validateexceldata.sql
@@ -5,114 +5,114 @@ BEGIN
 	
 	SET NOCOUNT ON;
 
-DECLARE
-	@MissingInformationError nvarchar(255) = 'Missing information in one or more fields',
-	@DuplicateInfoError nvarchar(255) = 'Duplicate of invoice',
-	@NoServicesError nvarchar(255) = 'No services performed',
-	@TotalNotMatchError nvarchar(255) = 'Amount of services combined does not equal total',
-	@NotValidTechnician nvarchar(255) = 'Cannot find Technician in table',
-	@NotValidCity nvarchar(255) = 'City not in Table',
-	@NotValidState nvarchar(255) = 'State not in Table',
-	@WrongZipcode nvarchar(255) = 'Wrong number of digits in zip code',
-	@PassedValidation nvarchar(255) = 'Passed Validation',
-	@FailedValidation nvarchar(255) = 'Failed Validation'
+	DECLARE
+		@MissingInfORmationError NVARCHAR(255) = 'Missing infORmation in one OR mORe fields',
+		@DuplicateInfoError NVARCHAR(255) = 'Duplicate of invoice',
+		@NoServicesErrOR NVARCHAR(255) = 'No services perfORmed',
+		@TotalNotMatchError NVARCHAR(255) = 'Amount of services combined does not equal total',
+		@NotValidTechnician NVARCHAR(255) = 'Cannot find Technician in table',
+		@NotValidCity NVARCHAR(255) = 'City not in Table',
+		@NotValidState NVARCHAR(255) = 'State not in Table',
+		@WrongZipcode NVARCHAR(255) = 'Wrong number of digits in zip code',
+		@PassedValidation NVARCHAR(255) = 'PASsed Validation',
+		@FailedValidation NVARCHAR(255) = 'Failed Validation'
 
---Everytime Validation is run, clean all previous errors off
-Truncate Table dbo.exceldataerror
+	--Everytime Validation is run, clean all previous errors off
+	TRUNCATE TABLE DBO.ExcelDataError
 
-	--Clean up empty spaces
-	Update dbo.Exceldata
-	set TechnicianName = LTRIM(RTRIM(Technicianname)),
+		--Clean up empty spaces
+	UPDATE DBO.ExcelData
+	SET TechnicianName = LTRIM(RTRIM(Technicianname)),
 	Addressnumber = LTRIM(RTRIM(Addressnumber)),
 	Street = LTRIM(RTRIM(Street)),
 	Apt = LTRIM(RTRIM(Apt)),
 	City = LTRIM(RTRIM(City)),
 	[State] = LTRIM(RTRIM(Technicianname))
 
---Missing Information
-Insert into dbo.exceldataerrors (ExceldataId, ErrorMessage)
-	Select rowid, @Missinginformationerror from dbo.exceldata e
-		Where (TechnicianName = '' or TechnicianName is null)
-		OR (Addressnumber = '' or Addressnumber is null)
-		OR (JobDate = '' or JobDate is null)
-		OR (Street = '' or Street is null)
-		OR (City = '' or City is Null)
-		OR ([State] = '' or [State] is Null)
-		OR (Zipcode = '' or Zipcode is Null)
-		OR (Total = '' or Total is Null or Total = 0)
+	--Missing Information
+	INSERT INTO DBO.ExcelDataErrors (ExcelDataId, ErrorMessage)
+		SELECT Rowid, @MissinginfORmationError FROM DBO.ExcelData
+			WHERE (TechnicianName = '' OR TechnicianName IS NULL)
+			OR (Addressnumber = '' OR Addressnumber IS NULL)
+			OR (JobDate = '' OR JobDate IS NULL)
+			OR (Street = '' OR Street IS NULL)
+			OR (City = '' OR City IS NULL)
+			OR ([State] = '' OR [State] IS NULL)
+			OR (Zipcode = '' OR Zipcode IS NULL)
+			OR (Total = '' OR Total IS NULL OR Total = 0)
+			
+	--Duplicate Invoices
+	INSERT INTO DBO.ExcelEataErrors	(ExcelDataId, ErrorMessage)
+	SELECT RowId, @DuplicateInfoerrOR FROM (
+				SELECT *, ROW_NUMBER() OVER	
+				(PARTITION BY Jobdate, TechnicianName, Street, Total 
+				ORDER BY Jobdate, TechnicianName, Street, Total) AS Rn
+		FROM DBO.ExcelData) x
+	WHERE x.rn > 1
+
+	--No services perfORmed
+	INSERT INTO DBO.ExcelDataErrors	(ExcelDataId, ErrorMessage)
+	SELECT Rowid, @NoserviceserrOR FROM DBO.ExcelData
+		WHERE (Carpet = '' OR Carpet IS NULL OR Carpet = 0)
+			and (UrineTreatment = '' OR UrineTreatment IS NULL OR UrineTreatment = 0)
+			and (Protectant = '' OR Protectant IS NULL OR Protectant = 0)
+			and (Upholstery = '' OR Upholstery IS NULL OR Upholstery = 0)
+			and (AirDucts = '' OR AirDucts IS NULL OR AirDucts = 0)
+			and (Tile = '' OR Tile IS NULL OR Tile = 0)
+			and (DryerVent = '' OR DryerVent IS NULL OR DryerVent = 0)
+			and (Other = '' OR Other IS NULL OR Other = 0)
+			
+	--Total does not match
+	INSERT INTO DBO.ExcelDataErrors	(ExcelDataId, ErrorMessage)
+	SELECT Rowid, @TotalNotMatchError FROM DBO.ExcelData
+		WHERE Total != (
+			ISNULL(Carpet, 0) +
+			ISNULL(UrineTreatment, 0) +
+			ISNULL(Protectant, 0) +
+			ISNULL(Upholstery, 0) +
+			ISNULL(Airducts, 0) +
+			ISNULL(Tile, 0) +
+			ISNULL(Dryervent, 0) +
+			ISNULL(Other, 0) -
+			ISNULL(Discount, 0))
+			
+	--Cannot find technician in table
+	INSERT INTO DBO.ExcelDataErrors	(ExcelDataId, ErrorMessage)
+	SELECT Rowid, @NotvalidTechnician 
+	FROM DBO.ExcelData e
+		LEFT JOIN DBO.technicians t 
+		on t.name = HASHBYTES('SHA2_256', Concat(e.TechnicianName,'Thisisnottherealsalt'))
+		WHERE t.TechnicianID IS NULL
 		
---Duplicate Invoices
-Insert into Dbo.exceldataerrors	(Exceldataid, ErrorMessage)
-Select RowId, @DuplicateInfoerror from (
-			Select *, ROW_NUMBER() over	
-			(partition by Jobdate, TechnicianName, Street, Total 
-			order by Jobdate, TechnicianName, Street, Total) as Rn
-	from dbo.Exceldata) x
-where x.rn > 1
+	--Cannot find City in table
+	INSERT INTO DBO.ExcelDataErrors	(ExcelDataId, ErrorMessage)
+	SELECT Rowid, @NotvalidCity FROM DBO.ExcelData e
+		LEFT JOIN DBO.Cities c 
+		on c.City = e.city
+		WHERE c.CityID IS NULL
 
---No services performed
-Insert into Dbo.exceldataerrors	(Exceldataid, ErrorMessage)
-Select Rowid, @Noserviceserror from dbo.Exceldata
-	where (Carpet = '' or Carpet is null or Carpet = 0)
-		and (Urinetreatment = '' or Urinetreatment is null or Urinetreatment = 0)
-		and (Protectant = '' or Protectant is null or Protectant = 0)
-		and (Upholstery = '' or Upholstery is null or Upholstery = 0)
-		and (AirDucts = '' or AirDucts is null or AirDucts = 0)
-		and (Tile = '' or Tile is null or Tile = 0)
-		and (DryerVent = '' or DryerVent is null or DryerVent = 0)
-		and (Other = '' or Other is null or Other = 0)
-		
---Total does not match
-Insert into Dbo.exceldataerrors	(Exceldataid, ErrorMessage)
-Select Rowid, @Totalnotmatcherror from dbo.Exceldata
-	where Total != (
-		ISNULL(Carpet, 0) +
-		ISNULL(Urinetreatment, 0) +
-		ISNULL(Protectant, 0) +
-		ISNULL(Upholstery, 0) +
-		ISNULL(Airducts, 0) +
-		ISNULL(Tile, 0) +
-		ISNULL(Dryervent, 0) +
-		ISNULL(Other, 0) -
-		ISNULL(Discount, 0))
-		
---Cannot find technician in table
-Insert into Dbo.exceldataerrors	(Exceldataid, ErrorMessage)
-Select Rowid, @NotvalidTechnician 
-from dbo.exceldata e
-	left join dbo.technicians t 
-	on t.name = Hashbytes('Sha2_256', Concat(e.TechnicianName,'Thisisnottherealsalt'))
-	where t.TechnicianID is null
-	   
---Cannot find City in table
-Insert into Dbo.exceldataerrors	(Exceldataid, ErrorMessage)
-Select Rowid, @NotvalidCity from dbo.Exceldata e
-	left join dbo.Cities c 
-	on c.City = e.city
-	where c.CityID is null
+	--City not in Missouri OR Illinois
+	INSERT INTO DBO.ExcelDataErrors ( ExcelDataId, ErrorMessage)
+	SELECT rowid, @NotvalidState FROM DBO.ExcelData e
+	WHERE [STATE] NOT IN ('MO','IL')
 
---City not in Missouri or Illinois
-Insert into Dbo.Exceldataerrors ( ExceldataId, ErrorMessage)
-Select rowid, @NotvalidState from dbo.Exceldata e
-where State not in ('MO','IL')
+	--Zip code wrong
+	INSERT INTO DBO.ExcelDataErrors ( ExcelDataId, ErrorMessage)
+	SELECT rowid, @WrongZipcode FROM DBO.ExcelData e
+	WHERE zipcode NOT LIKE '[6][0-9][0-9][0-9][0-9]'
 
---Zip code wrong
-Insert into Dbo.Exceldataerrors ( ExceldataId, ErrorMessage)
-Select rowid, @WrongZipcode from dbo.Exceldata e
-where zipcode not like '[6][0-9][0-9][0-9][0-9]'
+	--Passed Validation
+	UPDATE e
+	SET Loaddatanotes = @PASsedValidation
+	FROM DBO.ExcelData e
+	LEFT JOIN DBO.ExcelDataErrors edt on edt.ErrorId = e.RowId
+	WHERE edt.ErrorId IS NULL
 
---Passed Validation
-Update e
-Set Loaddatanotes = @PassedValidation
-from dbo.Exceldata e
-Left join dbo.Exceldataerrors edt on edt.Errorid = e.RowId
-where edt.Errorid is null
-
---Failed Validation
-Update e
-Set Loaddatanotes = @FailedValidation
-from dbo.Exceldata e
-Left join dbo.Exceldataerrors edt on edt.Errorid = e.RowId
+	--Failed Validation
+	UPDATE e
+	SET Loaddatanotes = @FailedValidation
+	FROM DBO.ExcelData e
+	LEFT JOIN DBO.ExcelDataErrors edt on edt.ErrorId = e.RowId
 
 END
 GO
